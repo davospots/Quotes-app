45386bb2171755c29e35e552e58791fc459688ec
Dave <david.mathaga@student.moringaschool.com>
2021-10-12 10:07:55 +0300

    capture value in realtime on forms

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,11 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  public formName: string = "";
-  public formPublisher: string = "";
-  public formQuote: string = "";
-  published!: Date;
-  newQuote: any;
+ 
 
   submitQuote(){
 
--- src/app/messages.ts
+++
@@ -1,5 +0,0 @@
-export class Messages {
-    constructor(public formName:string, public formPublisher:string, public formQuote:string, public published:Date){
-
-    }
-}
